3796ffc74a6066bcd69a163f6b720ecb3d7fbe8b
josefran <josefran@gmail.com>
2024-12-17 18:48:28 +0100

    Day 17 - WIP Part 2

--- src/main/java/aoc2024/Day17.java
+++ src/main/java/aoc2024/Day17.java
@@ -15,18 +15,26 @@ public class Day17 {
                 .collect(Collectors.joining(","));
     }
 
-    public int executePart2(String input) {
+    public long executePart2(String input) {
         Computer computer = createComputer(input);
         Program program = createProgram(input);
-        int aValue = computer.a.value() + 1;
-        boolean equals = false;
-        List<Integer> initial = initialList(program);
-        int threshold = initial.size();
-        while (!equals) {
-            //if (aValue % 10000 == 0) System.out.println("Trying with a = " + aValue);
-            List<Integer> result = computer.executeProgram(program, new Register(aValue), threshold);
-            equals = areEquals(initial, result);
-            if (!equals) aValue++;
+        List<Integer> expectedOutput = expectedOutput(program);
+
+        long aValue = computer.state().a() + 1;
+        boolean found = false;
+        while (!found) {
+            if (aValue % 10000000 == 0) System.out.println("Trying with a = " + aValue);
+            computer.reset(aValue, 0, 0);
+            program.reset();
+            List<Integer> output = computer.executeProgram(program, expectedOutput);
+            if (areEquals(expectedOutput, output)) {
+                found = true;
+            } else {
+                aValue++;
+            }
+//            System.out.println("A: " + aValue);
+//            System.out.println("State: " + computer.state());
+//            System.out.println("Outputs: " + output);
         }
         return aValue;
     }
@@ -39,7 +47,7 @@ public class Day17 {
         return true;
     }
 
-    private static List<Integer> initialList(Program program) {
+    private static List<Integer> expectedOutput(Program program) {
         List<Integer> initial = new ArrayList<>();
         for (Instruction instruction : program.instructions()) {
             initial.add(instruction.opcode());
@@ -61,9 +69,9 @@ public class Day17 {
     private static List<Instruction> parseInstructions(String instructionsString) {
         String[] split = instructionsString.split(",");
         List<Instruction> instructions = new ArrayList<>();
-        for (int i = 0; i < split.length; i+=2) {
+        for (int i = 0; i < split.length; i += 2) {
             int opcode = Integer.parseInt(split[i]);
-            int operand = Integer.parseInt(split[i+1]);
+            int operand = Integer.parseInt(split[i + 1]);
             instructions.add(new Instruction(opcode, operand));
         }
         return instructions;
@@ -72,55 +80,99 @@ public class Day17 {
     private Computer createComputer(String input) {
         List<String> list = input.lines().toList();
         Pattern pattern = Pattern.compile("Register ([A-C]): (\\d+)");
-        Map<Character, Integer> registers = new java.util.HashMap<>(Map.of('A', 0, 'B', 0, 'C', 0));
+        Map<Character, Long> registers = new java.util.HashMap<>(Map.of('A', 0L, 'B', 0L, 'C', 0L));
         for (int i = 0; i < 3; i++) {
             String line = list.get(i);
             Matcher matcher = pattern.matcher(line);
             if (!matcher.find()) throw new IllegalArgumentException("Invalid input");
             String letter = matcher.group(1);
-            int value = Integer.parseInt(matcher.group(2));
+            long value = Long.parseLong(matcher.group(2));
             registers.put(letter.charAt(0), value);
         }
-        return new Computer(
-                new Register(registers.get('A')),
-                new Register(registers.get('B')),
-                new Register(registers.get('C')));
+        Registers state = new Registers(registers.get('A'), registers.get('B'), registers.get('C'));
+        return new Computer(state);
     }
 
-    static final class Computer {
-        private Register a;
-        private Register b;
-        private Register c;
+    static class Registers {
+        private long a;
+        private long b;
+        private long c;
 
-        Computer(Register a, Register b, Register c) {
+        Registers(long a, long b, long c) {
             this.a = a;
             this.b = b;
             this.c = c;
         }
 
-        List<Integer> executeProgram(Program program, Register a, int threshold) {
+        public long a() {
+            return a;
+        }
+
+        public long b() {
+            return b;
+        }
+
+        public long c() {
+            return c;
+        }
+
+        public void setA(long a) {
             this.a = a;
-            this.b = new Register(0);
-            this.c = new Register(0);
-            program.reset();
-            return executeProgram(program, threshold);
+        }
+
+        public void setB(long b) {
+            this.b = b;
+        }
+
+        public void setC(long c) {
+            this.c = c;
+        }
+
+        @Override
+        public String toString() {
+            return "Registers[" +
+                    "a=" + a + ", " +
+                    "b=" + b + ", " +
+                    "c=" + c + ']';
+        }
+
+        public void reset(long a, long b, long c) {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+    }
+
+    static final class Computer {
+        private final Registers registers;
+
+        Computer(Registers initialRegisters) {
+            this.registers = initialRegisters;
+        }
+
+        Registers state() {
+            return registers;
+        }
+
+        @SuppressWarnings("SameParameterValue")
+        void reset(long a, long b, long c) {
+            registers.reset(a, b, c);
         }
 
         List<Integer> executeProgram(Program program) {
-            return executeProgram(program, Integer.MAX_VALUE);
+            return executeProgram(program, Collections.emptyList());
         }
 
-        List<Integer> executeProgram(Program program, int threshold) {
+        List<Integer> executeProgram(Program program, List<Integer> resultToCheck) {
             List<Integer> outputs = new ArrayList<>();
             Set<String> executedInstructions = new HashSet<>();
-
+            int threshold = resultToCheck.isEmpty() ? Integer.MAX_VALUE : resultToCheck.size();
             while (program.hasNext() && outputs.size() <= threshold) {
-                String state = program.instructionPointer() + "-" + a.value() + "-" + b.value() + "-" + c.value();
-                if (!executedInstructions.add(state)) {
+                String cache = program.instructionPointer() + "-" + registers.a() + "-" + registers.b() + "-" + registers.c();
+                if (!executedInstructions.add(cache)) {
                     System.out.println("Loop detected at instruction pointer: " + program.instructionPointer);
                     break;
                 }
-
                 Instruction instruction = program.next();
                 switch (instruction.opcode()) {
                     case 0 -> adv(instruction.operand());
@@ -132,76 +184,80 @@ public class Day17 {
                     case 6 -> bdv(instruction.operand());
                     case 7 -> cdv(instruction.operand());
                 }
+                if (!resultToCheck.isEmpty()) {
+                    boolean isSubsequence = isSubsequence(outputs, resultToCheck);
+                    if (!isSubsequence) break;
+                }
             }
             return outputs;
         }
 
+        private static boolean isSubsequence(List<Integer> outputs, List<Integer> resultToCheck) {
+            return resultToCheck.size() >= outputs.size()
+                    && resultToCheck.subList(0, outputs.size()).equals(outputs);
+        }
+
         private void adv(int operand) {
-            int value = division(operand);
-            a = new Register(value);
-        }
-
-        private int division(int operand) {
-            int numerator = a.value();
-            int denominator = (int) Math.pow(2, combo(operand));
-            return Math.floorDiv(numerator , denominator);
-        }
-
-        private void bxl(int operand) {
-            int value = b.value() ^ operand;
-            b = new Register(value);
-        }
-
-        private void bst(int operand) {
-            int value = combo(operand) % 8;
-            int valueLowest3bits = value & 0b111;
-            b = new Register(valueLowest3bits);
-        }
-
-        private void jnz(int operand, Program program) {
-            if (a.value() != 0) program.jump(operand);
-        }
-
-        private void bxc() {
-            int value = b.value() ^ c.value();
-            b = new Register(value);
-        }
-
-        private int out(int operand) {
-            return combo(operand) % 8;
+            long value = division(operand);
+            registers.setA(value);
         }
 
         private void bdv(int operand) {
-            int value = division(operand);
-            b = new Register(value);
+            long value = division(operand);
+            registers.setB(value);
         }
 
         private void cdv(int operand) {
-            int value = division(operand);
-            c = new Register(value);
+            long value = division(operand);
+            registers.setC(value);
         }
 
-        private int combo(int opcode) {
-            if (opcode < 0 || opcode > 6) {
-                throw new IllegalArgumentException("Invalid opcode in combo: " + opcode);
+        private long division(int operand) {
+            long numerator = state().a();
+            long denominator = (long) Math.pow(2, combo(operand));
+            return Math.floorDiv(numerator, denominator);
+        }
+
+        private void bxl(int operand) {
+            long value = state().b() ^ operand;
+            registers.setB(value);
+        }
+
+        private void bst(int operand) {
+            long value = combo(operand) % 8;
+            long valueLowest3bits = value & 0b111;
+            registers.setB(valueLowest3bits);
+        }
+
+        private void jnz(int operand, Program program) {
+            if (state().a() != 0) program.jump(operand);
+        }
+
+        private void bxc() {
+            long value = state().b() ^ state().c();
+            registers.setB(value);
+        }
+
+        private int out(int operand) {
+            return (int) combo(operand) % 8;
+        }
+
+        private long combo(int operand) {
+            if (operand < 0 || operand > 6) {
+                throw new IllegalArgumentException("Invalid operand in combo: " + operand);
             }
-            return switch (opcode) {
-                case 4 -> a.value();
-                case 5 -> b.value();
-                case 6 -> c.value();
-                default -> opcode;
+            return switch (operand) {
+                case 4 -> state().a();
+                case 5 -> state().b();
+                case 6 -> state().c();
+                default -> operand;
             };
         }
-
-    }
-
-    record Register(int value) {
-
     }
 
     static final class Program {
         private final List<Instruction> instructions;
-        int instructionPointer = 0;
+        private int instructionPointer = 0;
 
         Program(List<Instruction> instructions) {
             this.instructions = new ArrayList<>(instructions);
@@ -233,7 +289,6 @@ public class Day17 {
     }
 
     record Instruction(int opcode, int operand) {
-
         Instruction {
             if (opcode < 0 || opcode > 7) {
                 throw new IllegalArgumentException("Invalid opcode: " + opcode);
--- src/test/java/aoc2024/Day17Test.java
+++ src/test/java/aoc2024/Day17Test.java
@@ -4,6 +4,8 @@ import aoc2024.utils.InputProvider;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class Day17Test {
@@ -40,7 +42,7 @@ public class Day17Test {
     @Test
     void testPart1SimpleCase() {
         String input = """
-                Register A: 2024
+                Register A: 117440
                 Register B: 0
                 Register C: 0
                 
@@ -65,19 +67,94 @@ public class Day17Test {
         assertThat(result).isEqualTo("2,7,6,5,6,0,2,3,1");
     }
 
+    @Test
+    void testPart2Example() {
+        long result = new Day17().executePart2(part2ExampleInput());
+        System.out.println(result);
+        assertThat(result).isEqualTo(117440);
+    }
 
     @Disabled
     @Test
-    void testPart2Example() {
-        int result = new Day17().executePart2(part2ExampleInput());
+    void testPart2() {
+        long result = new Day17().executePart2(input());
         System.out.println(result);
-        assertThat(result).isEqualTo(117440);
+        assertThat(result).isEqualTo(33940147);
+    }
+
+
+    @Test
+    void testCases1() {
+        // 1. If register C contains 9, the program 2,6 would set register B to 1.
+        Day17.Registers registers = new Day17.Registers(0, 0, 9);
+        Day17.Computer computer = new Day17.Computer(registers);
+        Day17.Program program = new Day17.Program(List.of(new Day17.Instruction(2, 6)));
+
+        computer.executeProgram(program);
+
+        assertThat(computer.state().b()).isEqualTo(1);
     }
 
     @Test
-    void testPart2() {
-        int result = new Day17().executePart2(input());
-        System.out.println(result);
-        assertThat(result).isEqualTo(117440);
+    void testCases2() {
+        // 2. If register A contains 10, the program 5,0,5,1,5,4 would output 0,1,2.
+        Day17.Registers registers = new Day17.Registers(10, 0, 0);
+        Day17.Computer computer = new Day17.Computer(registers);
+        Day17.Program program = new Day17.Program(List.of(
+                new Day17.Instruction(5, 0),
+                new Day17.Instruction(5, 1),
+                new Day17.Instruction(5, 4)
+        ));
+
+        List<Integer> integers = computer.executeProgram(program);
+
+        assertThat(integers).containsExactly(0, 1, 2);
     }
+
+    @Test
+    void testCases3() {
+        // 3. If register A contains 2024, the program 0,1,5,4,3,0 would output 4,2,5,6,7,7,7,7,3,1,0 and leave 0 in register A.
+        Day17.Registers registers = new Day17.Registers(2024, 0, 0);
+        Day17.Computer computer = new Day17.Computer(registers);
+        Day17.Program program = new Day17.Program(List.of(
+                new Day17.Instruction(0, 1),
+                new Day17.Instruction(5, 4),
+                new Day17.Instruction(3, 0)
+        ));
+
+        List<Integer> integers = computer.executeProgram(program);
+
+        assertThat(integers).containsExactly(4, 2, 5, 6, 7, 7, 7, 7, 3, 1, 0);
+        assertThat(computer.state().a()).isEqualTo(0);
+    }
+
+    @Test
+    void testCases4() {
+        // 4. If register B contains 29, the program 1,7 would set register B to 26.
+        Day17.Registers registers = new Day17.Registers(0, 29, 0);
+        Day17.Computer computer = new Day17.Computer(registers);
+        Day17.Program program = new Day17.Program(List.of(
+                new Day17.Instruction(1, 7)
+
+        ));
+
+        computer.executeProgram(program);
+
+        assertThat(computer.state().b()).isEqualTo(26);
+    }
+
+    @Test
+    void testCases5() {
+        // 5. If register B contains 2024 and register C contains 43690, the program 4,0 would set register B to 44354.
+        Day17.Registers registers = new Day17.Registers(0, 2024, 43690);
+        Day17.Computer computer = new Day17.Computer(registers);
+        Day17.Program program = new Day17.Program(List.of(
+                new Day17.Instruction(4, 0)
+        ));
+
+        computer.executeProgram(program);
+
+        assertThat(computer.state().b()).isEqualTo(44354);
+    }
+
 }
